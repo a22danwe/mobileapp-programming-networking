20b58a98706f1f29a60ea4e22d8b723d2e96dc13
a22danwe <a22danwe@student.his.se>
2023-05-16 13:09:46 +0200

    har varit tvungen att kopiera in koden i nytt projekt då android studio krånglar.

--- app/src/main/java/com/example/networking/Mountain.java
+++ app/src/main/java/com/example/networking/Mountain.java
@@ -1,4 +1,23 @@
 package com.example.networking;
 
+import android.util.Log;
+
 public class Mountain {
+
+    private String name;
+    private String location;
+    private int height;
+
+    public Mountain(String name, String location, int height) {
+
+        this.name = name;
+        this.location = location;
+        this.height = height;
+    }
+
+    public String toString() {
+        String tmp = name + " " + location + " " + height;
+
+        return tmp;
+    }
 }
